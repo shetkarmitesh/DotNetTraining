6748146190a255e4c5605cdb77647bf6eb54f58b
shetkarmitesh <miteshshetkar3125@gmail.com>
2024-06-12 03:59:28 +0530

    #

--- Assignment4/VisitorSecurityClearanceSystem/Services/EmailSender.cs
+++ Assignment4/VisitorSecurityClearanceSystem/Services/EmailSender.cs
@@ -8,22 +8,23 @@ namespace VisitorSecurityClearanceSystem.Services
 {
     public class EmailSender
     {
-        public async Task SendEmail(string subject,string toEmail, string userName,string message, byte[] pdfBytes=null)
-        {
-            var apiKey = Credentials.ApiKey;
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("tempvirajtapkir1800@gmail.com", "VisitorSecurityClearanceSystem");
-            var to = new EmailAddress(toEmail, userName);
-            var plainTextContent = message;
-            var htmlContent = "";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        /* public async Task SendEmail(string subject,string toEmail, string userName,string message, byte[] pdfBytes=null)
+         {
+             var apiKey = Credentials.ApiKey;
+             var client = new SendGridClient(apiKey);
+             var from = new EmailAddress("tempvirajtapkir1800@gmail.com", "VisitorSecurityClearanceSystem");
+             var to = new EmailAddress(toEmail, userName);
+             var plainTextContent = message;
+             var htmlContent = "";
+             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
-            // Attach the PDF if provided
-            if (pdfBytes != null)
-            {
-                msg.AddAttachment("VisitorPass.pdf", Convert.ToBase64String(pdfBytes));
-            }
-            var response = await client.SendEmailAsync(msg);
-        }   
+             // Attach the PDF if provided
+             if (pdfBytes != null)
+             {
+                 msg.AddAttachment("VisitorPass.pdf", Convert.ToBase64String(pdfBytes));
+             }
+             var response = await client.SendEmailAsync(msg);
+        */
     }
+}
 }
