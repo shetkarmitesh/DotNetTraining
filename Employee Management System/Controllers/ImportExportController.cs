0d4559d0adc8dd5c50e897581be3182cad53a00a
shetkarmitesh <miteshshetkar3125@gmail.com>
2024-06-26 14:47:06 +0530

    Ass week 8 : export ExportAllBasicAndAdditionalDetails

--- Employee Management System/Controllers/ImportExportController.cs
+++ Employee Management System/Controllers/ImportExportController.cs
@@ -146,5 +146,221 @@ namespace Employee_Management_System.Controllers
                 
             }
         }
+        /*
+                [HttpGet]
+                 public async Task<IActionResult> ExportAllBasicAndAdditionalDetails()
+                 {
+                     var basicDetails = await _employeeBasicDetailsService.GetAllEmployeeBasicDetails();
+
+                     var additionalDetails = await _employeeAdditionalDetailsService.GetAllEmployeeAdditionalDetails();
+
+
+                     *//*var employeesData = from basic in basicDetails
+                                         join additional in additionalDetails on basic.EmployeeID equals additional.EmployeeBasicDetailsUId
+                                         select new
+                                         {
+
+                                             EmployeeID = basic.EmployeeID,
+                                             Salutory = basic.Salutory,
+                                             FirstName = basic.FirstName,
+                                             MiddleName = basic.MiddleName,
+                                             LastName = basic.LastName,
+                                             Email = basic.Email,
+                                             AlternateEmail = additional.AlternateEmail,
+                                             Phone = basic.Mobile,
+                                             AlternatePhone=additional.AlternateMobile,
+                                             Role = basic.Role,  
+                                             ReportingManagerUID= basic.ReportingManagerUId,
+                                             ReportingManagerName = basic.ReportingManagerName,
+                                             Address = $"{basic.Address.HouseNumber}, {basic.Address.SocietyName}, {basic.Address.Pincode}, {basic.Address.City}, {basic.Address.State}, {basic.Address.Country}",
+                                            *//* DateOfBirth = additional.PersonalDetails?.DateOfBirth.ToString("yyyy-MM-dd"),
+                                             DateOfJoining = additional.WorkInformation?.DateOfJoining.ToString("yyyy-MM-dd"),*//*
+                                             WorkInformation = additional.WorkInformation != null ? $"{additional.WorkInformation.DesignationName}, {additional.WorkInformation.DepartmentName}, {additional.WorkInformation.LocationName}, {additional.WorkInformation.EmployeeStatus}, {additional.WorkInformation.SourceOfHire}" : "",
+                                             // PersonalDetails as formatted string
+                                             PersonalDetails = additional.PersonalDetails != null ? $"{additional.PersonalDetails?.DateOfBirth.ToString("yyyy-MM-dd")}, {additional.PersonalDetails.Age}, {additional.PersonalDetails.Religion}, {additional.PersonalDetails.Caste}, {additional.PersonalDetails.MaritalStatus}, {additional.PersonalDetails.BloodGroup}, {additional.PersonalDetails.Height}, {additional.PersonalDetails.Weight}" : "",
+                                             // IdentityInformation as formatted string
+                                             IdentityInformation = additional.IdentityInformation != null ? $"{additional.IdentityInformation.Aadhar}, {additional.IdentityInformation.Nationality}, {additional.IdentityInformation.PassportNumber}, {additional.IdentityInformation.PFNumber}" : "",
+                                         };
+         *//*
+                     var employeesData = from basic in basicDetails
+                                         join additional in additionalDetails on basic.EmployeeID equals additional.EmployeeBasicDetailsUId
+                                         select new
+                                         {
+                                             basic,
+                                             additional.WorkInformation,
+                                             additional.PersonalDetails,
+                                             additional.IdentityInformation,
+                                         };
+
+                     ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+                     using (var package = new ExcelPackage())
+                     {
+                         var worksheet = package.Workbook.Worksheets.Add("EmployeesDetails");
+                         //headers
+                         var rowIndex = 1;
+                         int colIndex = 1;
+                         // Basic details headers
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Sr.No";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "EmployeeID";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Salutory";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "First Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Middle Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Last Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Email";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Alternate Email";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Phone No";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Alternate Phone No";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Role";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Reporting Manager UID";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Reporting Manager Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Address";
+
+                         // Work information headers (assuming properties)
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Designation Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Department Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Location Name";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Employee Status";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Source Of Hire";
+
+                         // Personal details headers (assuming properties)
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Date Of Birth";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Age";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Religion";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Caste";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Marital Status";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Blood Group";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Height";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Weight";
+
+                         // Identity information headers (assuming properties)
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Aadhar";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Nationality";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "Passport Number";
+                         worksheet.Cells[rowIndex, colIndex++].Value = "PF Number";
+
+                         rowIndex++;
+                         foreach (var data in employeesData)
+                         {
+                             worksheet.Cells[rowIndex, 1].Value = rowIndex - 1;
+                             worksheet.Cells[rowIndex, 2].Value = data.FirstName;
+                             worksheet.Cells[rowIndex, 2].Value = data.MiddleName;
+                             worksheet.Cells[rowIndex, 3].Value = data.LastName;
+                             worksheet.Cells[rowIndex, 4].Value = data.Email;
+                             worksheet.Cells[rowIndex, 4].Value = data.AlternateEmail;
+                             worksheet.Cells[rowIndex, 5].Value = data.Phone;
+                             worksheet.Cells[rowIndex, 5].Value = data.AlternatePhone;
+                             worksheet.Cells[rowIndex, 10].Value = data.Role;
+                             worksheet.Cells[rowIndex, 11].Value = data.ReportingManagerUID;
+                             worksheet.Cells[rowIndex, 12].Value = data.ReportingManagerName;
+                             worksheet.Cells[rowIndex, 13].Value = data.Address;
+                             *//*worksheet.Cells[rowIndex, 14].Value = data.DateOfBirth;
+                             worksheet.Cells[rowIndex, 15].Value = data.DateOfJoining;*//*
+                             worksheet.Cells[rowIndex, 14].Value = data.WorkInformation;
+                             worksheet.Cells[rowIndex, 15].Value = data.PersonalDetails;
+                             worksheet.Cells[rowIndex, 16].Value = data.IdentityInformation;
+                             rowIndex++;
+                         }
+
+                         var stream = new MemoryStream();
+                         package.SaveAs(stream);
+                         stream.Position = 0;
+                         return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "employeesDetails.xlsx");
+
+                     }
+
+
+                     }*/
+
+        [HttpGet]
+        public async Task<IActionResult> ExportAllBasicAndAdditionalDetails()
+        {
+            var basicDetails = await _employeeBasicDetailsService.GetAllEmployeeBasicDetails();
+            var additionalDetails = await _employeeAdditionalDetailsService.GetAllEmployeeAdditionalDetails();
+
+            var employeesData = from basic in basicDetails
+                                join additional in additionalDetails on basic.EmployeeID equals additional.EmployeeBasicDetailsUId
+                                select new
+                                {
+                                    basic,
+                                    additional,
+                                    additional.WorkInformation,
+                                    additional.PersonalDetails,
+                                    additional.IdentityInformation,
+                                };
+
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("EmployeesDetails");
+
+                // Headers
+                var headers = new string[]
+                {
+            "Sr.No", "EmployeeID", "Salutory", "First Name", "Middle Name", "Last Name",
+            "Email", "Alternate Email", "Phone No", "Alternate Phone No", "Role",
+            "Reporting Manager UID", "Reporting Manager Name", "Address",
+            "Designation Name", "Department Name", "Location Name", "Employee Status", "Source Of Hire",
+            "Date Of Birth", "Age", "Religion", "Caste", "Marital Status", "Blood Group", "Height", "Weight",
+            "Aadhar", "Nationality", "Passport Number", "PF Number"
+                };
+
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = headers[i];
+                }
+
+                int rowIndex = 2;
+                foreach (var data in employeesData)
+                {
+                    worksheet.Cells[rowIndex, 1].Value = rowIndex - 1;
+                    worksheet.Cells[rowIndex, 2].Value = data.basic.EmployeeID;
+                    worksheet.Cells[rowIndex, 3].Value = data.basic.Salutory;
+                    worksheet.Cells[rowIndex, 4].Value = data.basic.FirstName;
+                    worksheet.Cells[rowIndex, 5].Value = data.basic.MiddleName;
+                    worksheet.Cells[rowIndex, 6].Value = data.basic.LastName;
+                    worksheet.Cells[rowIndex, 7].Value = data.basic.Email;
+                    worksheet.Cells[rowIndex, 8].Value = data.additional.AlternateEmail;
+                    worksheet.Cells[rowIndex, 9].Value = data.basic.Mobile;
+                    worksheet.Cells[rowIndex, 10].Value = data.additional.AlternateMobile;
+                    worksheet.Cells[rowIndex, 11].Value = data.basic.Role;
+                    worksheet.Cells[rowIndex, 12].Value = data.basic.ReportingManagerUId;
+                    worksheet.Cells[rowIndex, 13].Value = data.basic.ReportingManagerName;
+                    worksheet.Cells[rowIndex, 14].Value = $"{data.basic.Address.HouseNumber}, {data.basic.Address.SocietyName}, {data.basic.Address.Pincode}, {data.basic.Address.City}, {data.basic.Address.State}, {data.basic.Address.Country}";
+
+                    // Work information
+                    worksheet.Cells[rowIndex, 15].Value = data.WorkInformation?.DesignationName;
+                    worksheet.Cells[rowIndex, 16].Value = data.WorkInformation?.DepartmentName;
+                    worksheet.Cells[rowIndex, 17].Value = data.WorkInformation?.LocationName;
+                    worksheet.Cells[rowIndex, 18].Value = data.WorkInformation?.EmployeeStatus;
+                    worksheet.Cells[rowIndex, 19].Value = data.WorkInformation?.SourceOfHire;
+
+                    // Personal details
+                    worksheet.Cells[rowIndex, 20].Value = data.PersonalDetails?.DateOfBirth.ToString("yyyy-MM-dd");
+                    worksheet.Cells[rowIndex, 21].Value = data.PersonalDetails?.Age;
+                    worksheet.Cells[rowIndex, 22].Value = data.PersonalDetails?.Religion;
+                    worksheet.Cells[rowIndex, 23].Value = data.PersonalDetails?.Caste;
+                    worksheet.Cells[rowIndex, 24].Value = data.PersonalDetails?.MaritalStatus;
+                    worksheet.Cells[rowIndex, 25].Value = data.PersonalDetails?.BloodGroup;
+                    worksheet.Cells[rowIndex, 26].Value = data.PersonalDetails?.Height;
+                    worksheet.Cells[rowIndex, 27].Value = data.PersonalDetails?.Weight;
+
+                    // Identity information
+                    worksheet.Cells[rowIndex, 28].Value = data.IdentityInformation?.Aadhar;
+                    worksheet.Cells[rowIndex, 29].Value = data.IdentityInformation?.Nationality;
+                    worksheet.Cells[rowIndex, 30].Value = data.IdentityInformation?.PassportNumber;
+                    worksheet.Cells[rowIndex, 31].Value = data.IdentityInformation?.PFNumber;
+
+                    rowIndex++;
+                }
+
+                // Stream the Excel package to the client as a file download
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "employeesDetails.xlsx");
+            }
+        }
+
+
     }
 }
